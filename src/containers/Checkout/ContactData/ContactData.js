59c20b634b91c844553980ec06c932dfeefc7f55
John Huynh <jhnhnyh9@gmail.com>
2018-10-08 14:41:15 -0700

    dynamically generate the input fields and set the two bindings and connected to the post in firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,34 +8,78 @@ import Input from '../../../components/UI/Input/Input'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Zip Code'
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        { value: 'fastest', displayValue: 'Fastest' },
+                        { value: 'cheapest', displayValue: 'Cheapest' }
+                    ]
+                },
+                value: ''
+            }
         },
         loading: false
     }
 
     orderHandler = (event) => {
         event.preventDefault();
-        console.log(this.props.ingredients)
+        // console.log(this.props.ingredients)
 
         this.setState({ loading: true })
+
+        const formData = {}
+        for ( let formElementIdentifier in this.state.orderForm ) {
+            formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
+        }
+
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'John Doe',
-                address: {
-                    street: '11 Test',
-                    zipCode: 123456,
-                    country: 'USA'
-                },
-                email: 'test@test.com',
-                deliveryMethod: 'fastest'
-            }
+            orderData: formData
         }
+
         axios.post('/orders.json', order)
             .then(response => {
                 this.setState({ loading: false })
@@ -46,12 +90,43 @@ class ContactData extends Component {
         // firebase uses a mongoDB like db so its needs to be in json
     }
 
+    inputChangedHandler = (event, inputIdentifier) => {
+        // console.log(event.target.value)
+        // since the copy is a pointer, the changes will change original state
+        // which is not the immutable pratice we want
+        // so we are doing two level cloning of the spread operate to
+        // make true clones of the state
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+
+        const updatedFormElement = {
+            ...updatedOrderForm[inputIdentifier]
+        }
+
+        updatedFormElement.value = event.target.value
+        updatedOrderForm[inputIdentifier] = updatedFormElement
+        this.setState({ orderForm: updatedOrderForm })
+    }
+
     render() {
-        let form = (<form>
-            <Input inputtype='input' type='text' name='name' placeholder='Your name' />
-            <Input inputtype='input' type='text' name='email' placeholder='Your email' />
-            <Input inputtype='input' type='text' name='street' placeholder='Street' />
-            <Input inputtype='input' type='text' name='postal' placeholder='Postal Code' />
+        const formElementsArray = []
+        for ( let key in this.state.orderForm ) {
+            formElementsArray.push({ 
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        let form = (<form onSubmit={this.orderHandler} >
+            {formElementsArray.map(formElement => (
+                <Input 
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    key={formElement.id}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} /> 
+            ))}
             <Button 
                 btnType='Success'
                 clicked={this.orderHandler}>ORDER</Button>
